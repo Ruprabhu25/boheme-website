e3c2e02a960ff6e9c5be589725686f786d0293e0
Kelly Phan <kelly@campus-034-054.ucdavis.edu>
2023-08-10 17:14:52 -0700

    Changes from px to vx, minor changes, and new page

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+ 
+app = Flask(__name__)  
+
+@app.route('/')
+def home():
+    return render_template("homepage.html")
+
+@app.route('/contact', methods = ["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        first_name = request.form.get("first name")
+        last_name = request.form.get("last name")
+        email = request.form.get("email")
+        reference = request.form.get("reference")
+        message = request.form.get("message")
+        
+        return "Your name is " + first_name + last_name
+    return render_template("contact.html")
+
+@app.route('/trade')
+def trade():
+    return render_template("trade.html")
+
+if __name__=='__main__':
+   app.run()
